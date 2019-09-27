a720754105614f001b1def4e5c621d888d055f94
guohaining <33090502+guohaining@users.noreply.github.com>
2019-09-28 00:36:24 +0800

    上传

---
+++ exam2_X5726/js/index.js
@@ -0,0 +1,11 @@
+window.onload = function() {
+    var app = {
+        init: function() {
+            this._render.call(this);
+        },
+        _render: function() {
+            
+        }
+    };
+    app.init();
+};
